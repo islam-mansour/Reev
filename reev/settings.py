d78d7480e4df0efd4d164785fb8f7b1f79aa2c9c
Islam Mansour <islammansour06@gmail.com>
2021-04-21 16:35:58 +0200

    resolve

--- reev/settings.py
+++ reev/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['*']
 
 ALLOWED_HOSTS = []
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
