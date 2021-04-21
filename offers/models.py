afc16db13c07ab5cc5310e601dddbcf0408101f6
Islam Mansour <islammansour06@gmail.com>
2021-04-21 18:07:03 +0200

    test

--- offers/models.py
+++ offers/models.py
@@ -5,3 +5,6 @@ class Offer(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     expired = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
